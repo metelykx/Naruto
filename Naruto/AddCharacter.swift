2cb67ac01579e78dcc3d31da78ab8ac8da2ac711
metelykx <ivaschenkoodenys@gmail.com>
2025-07-17 11:58:33 +0300

    comment for dismiss

--- Naruto/AddCharacter.swift
+++ Naruto/AddCharacter.swift
@@ -12,9 +12,10 @@ struct AddCharacter: View {
 
     //for get info from CoreData and save info
     @Environment(\.managedObjectContext) private var viewContext
-
     
+    //you can close screen after add the heroes
     @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
         Text("dd")
     }
