6718893dd1fb33177522226617d086e80a8041ce
metelykx <ivaschenkoodenys@gmail.com>
2025-07-17 13:54:46 +0300

    new struct for enemies Naruto

---
+++ Naruto/View/EnemiesView.swift
@@ -0,0 +1,18 @@
+//
+//  EnemiesView.swift
+//  Naruto
+//
+//  Created by Denis Ivaschenko on 17.07.2025.
+//
+
+import SwiftUI
+
+struct EnemiesView: View {
+    var body: some View {
+        Text("fhfh")
+    }
+}
+
+#Preview {
+    EnemiesView()
+}
