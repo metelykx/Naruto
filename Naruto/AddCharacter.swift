1f5a4573e39c036e8ac21ae96013bb86374674c9
metelykx <ivaschenkoodenys@gmail.com>
2025-07-17 12:19:26 +0300

    new comm

--- Naruto/AddCharacter.swift
+++ Naruto/AddCharacter.swift
@@ -16,14 +16,13 @@ struct AddCharacter: View {
     //you can close screen after add the heroes
     @Environment(\.dismiss) private var dismiss
     
-    
+    //variables for add heroes
     @State private var name: String = ""
     @State private var clan: String = ""
     @State private var element: String = ""
     @State private var village: String = ""
     @State private var imageURL: String = ""
     @State private var specialPower: String = ""
-    
     
     @State private var taidzusu: Int64 = 0
     @State private var ninjutsu: Int64 = 0
