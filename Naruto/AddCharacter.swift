4122a207ca4d464507420cb48e5cc6b1543eee37
metelykx <ivaschenkoodenys@gmail.com>
2025-07-17 11:40:50 +0300

    new struct for add new heroes

---
+++ Naruto/AddCharacter.swift
@@ -0,0 +1,14 @@
+//
+//  AddCharacter.swift
+//  Naruto
+//
+//  Created by Denis Ivaschenko on 17.07.2025.
+//
+
+import SwiftUI
+
+struct AddCharacter: View {
+    var body: some View {
+        Text("dd")
+    }
+}
