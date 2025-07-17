e89629677297f67a650425a2667feeee218146f7
metelykx <ivaschenkoodenys@gmail.com>
2025-07-17 11:57:16 +0300

    add dismiss

--- Naruto/AddCharacter.swift
+++ Naruto/AddCharacter.swift
@@ -10,9 +10,11 @@ import SwiftUI
 struct AddCharacter: View {
     
 
-    //for get info from CoreData and save info 
+    //for get info from CoreData and save info
     @Environment(\.managedObjectContext) private var viewContext
 
+    
+    @Environment(\.dismiss) private var dismiss
     var body: some View {
         Text("dd")
     }
