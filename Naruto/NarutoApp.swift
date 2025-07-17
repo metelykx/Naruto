c7068e4421094040e6bc0b2e2a9d94e772bc1f81
metelykx <ivaschenkoodenys@gmail.com>
2025-07-17 11:10:06 +0300

    Initial Commit

---
+++ Naruto/NarutoApp.swift
@@ -0,0 +1,20 @@
+//
+//  NarutoApp.swift
+//  Naruto
+//
+//  Created by Denis Ivaschenko on 17.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct NarutoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ NarutoTests/NarutoTests.swift
@@ -0,0 +1,17 @@
+//
+//  NarutoTests.swift
+//  NarutoTests
+//
+//  Created by Denis Ivaschenko on 17.07.2025.
+//
+
+import Testing
+@testable import Naruto
+
+struct NarutoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
