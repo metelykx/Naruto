5aaf9715f6007691243559321a617fb5d19e0f93
metelykx <ivaschenkoodenys@gmail.com>
2025-07-17 14:46:25 +0300

    new struct as BlockCharacterView

---
+++ Naruto/View/BlockCharacterView.swift
@@ -0,0 +1,18 @@
+//
+//  BlockCharacterView.swift
+//  Naruto
+//
+//  Created by Denis Ivaschenko on 17.07.2025.
+//
+
+import SwiftUI
+
+struct BlockCharacterView: View {
+    var body: some View {
+        Text("he")
+    }
+}
+
+#Preview {
+    BlockCharacterView()
+}
