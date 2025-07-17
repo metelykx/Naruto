377e0d41fd37e121c9ae25bb777ed9170f44e492
metelykx <ivaschenkoodenys@gmail.com>
2025-07-17 13:40:43 +0300

    new struct settingsVIew

---
+++ Naruto/SettingsView.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsView.swift
+//  Naruto
+//
+//  Created by Denis Ivaschenko on 17.07.2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("ffjfjfjf")
+    }
+}
+
+
+#Preview {
+    SettingsView()
+}
