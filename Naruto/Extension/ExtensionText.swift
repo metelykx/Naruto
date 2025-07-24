1b8941d4bb961ae1d1420140a0ceab43d4b10d90
metelykx <ivaschenkoodenys@gmail.com>
2025-07-24 21:49:45 +0300

    description

--- Naruto/Extension/ExtensionText.swift
+++ Naruto/Extension/ExtensionText.swift
@@ -17,4 +17,10 @@ extension Text {
             .font(.system(size: 16, weight: .bold, design: .rounded))
             .opacity(0.8)
     }
+    
+    func descriptionFont() -> some View {
+        self
+            .foregroundStyle(.primary)
+            .font(.system(size: 14, weight: .medium, design: .rounded))
+    }
 }
