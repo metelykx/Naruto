8fa4f9778c11701316f1485a746472946595d888
metelykx <ivaschenkoodenys@gmail.com>
2025-07-24 22:59:50 +0300

    add Zstack

--- Naruto/View/CardView.swift
+++ Naruto/View/CardView.swift
@@ -12,12 +12,14 @@ struct CardView: View {
     var body: some View {
         GeometryReader { geometry in
             VStack {
-                
-                Rectangle()
-                    .cornerRadius(20)
-                    .frame(height: geometry.size.height / 2)
-                    .padding(.top)
-                
+                ZStack {
+                    
+                    Rectangle()
+                        .cornerRadius(20)
+                        .frame(height: geometry.size.height / 2)
+                        .padding(.top)
+                        .padding(.horizontal)
+                }
                 
             }
         }
